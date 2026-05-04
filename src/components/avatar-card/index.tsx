e3d3b9e4f07eeb1b1b217e3e87991e25ac34c826
JI Ratul <72037035+ratul0547@users.noreply.github.com>
2026-05-04 12:40:13 -0500

    Implement theme toggle in AvatarCard
    Added theme toggle functionality to AvatarCard component.

--- src/components/avatar-card/index.tsx
+++ src/components/avatar-card/index.tsx
@@ -2,6 +2,139 @@ import { FALLBACK_IMAGE } from '../../constants';
 import { Profile } from '../../interfaces/profile';
 import { skeleton } from '../../utils';
 import LazyImage from '../lazy-image';
+import { RiMoonLine, RiSunLine } from 'react-icons/ri';
+import { LOCAL_STORAGE_KEY_NAME } from '../../constants';
+
+interface AvatarCardProps {
+  profile: Profile | null;
+  loading: boolean;
+  avatarRing: boolean;
+  resumeFileUrl?: string;
+  theme?: string;
+  setTheme?: (theme: string) => void;
+}
+
+/**
+ * Renders an AvatarCard component.
+ * @param profile - The profile object.
+ * @param loading - A boolean indicating if the profile is loading.
+ * @param avatarRing - A boolean indicating if the avatar should have a ring.
+ * @param resumeFileUrl - The URL of the resume file.
+ * @returns JSX element representing the AvatarCard.
+ */
+const AvatarCard: React.FC<AvatarCardProps> = ({
+  profile,
+  loading,
+  avatarRing,
+  resumeFileUrl,
+  theme,
+  setTheme,
+}): React.JSX.Element => {
+  const toggleTheme = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!setTheme || !theme) return;
+
+    const selectedTheme = theme === 'light' ? 'dark' : 'light';
+
+    document.querySelector('html')?.setAttribute('data-theme', selectedTheme);
+
+    typeof window !== 'undefined' &&
+      localStorage.setItem(LOCAL_STORAGE_KEY_NAME, selectedTheme);
+
+    setTheme(selectedTheme);
+  };
+
+  return (
+    <div className="card shadow-lg card-sm bg-base-100 relative">
+      {theme && setTheme && (
+        <button
+          onClick={toggleTheme}
+          title="Toggle Theme"
+          className="btn btn-ghost btn-sm btn-circle absolute top-4 right-4 z-10 normal-case text-base-content opacity-50"
+        >
+          {theme === 'light' ? (
+            <RiMoonLine className="w-5 h-5 stroke-current" />
+          ) : (
+            <RiSunLine className="w-5 h-5 stroke-current" />
+          )}
+        </button>
+      )}
+      <div className="grid place-items-center py-8">
+        {loading || !profile ? (
+          <div className="avatar opacity-90">
+            <div className="mb-8 rounded-full w-32 h-32">
+              {skeleton({
+                widthCls: 'w-full',
+                heightCls: 'h-full',
+                shape: '',
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="avatar opacity-90">
+            <div
+              className={`mb-8 rounded-full w-32 h-32 ${
+                avatarRing
+                  ? 'ring-3 ring-primary ring-offset-base-100 ring-offset-2'
+                  : ''
+              }`}
+            >
+              {
+                <LazyImage
+                  src={profile.avatar ? profile.avatar : FALLBACK_IMAGE}
+                  alt={profile.name}
+                  placeholder={skeleton({
+                    widthCls: 'w-full',
+                    heightCls: 'h-full',
+                    shape: '',
+                  })}
+                />
+              }
+            </div>
+          </div>
+        )}
+        <div className="text-center mx-auto px-8">
+          <h5 className="font-bold text-2xl">
+            {loading || !profile ? (
+              skeleton({ widthCls: 'w-48', heightCls: 'h-8' })
+            ) : (
+              <span className="text-base-content opacity-70">
+                {profile.name}
+              </span>
+            )}
+          </h5>
+          <div className="mt-3 text-base-content font-mono">
+            {loading || !profile
+              ? skeleton({ widthCls: 'w-48', heightCls: 'h-5' })
+              : profile.bio}
+          </div>
+        </div>
+        {resumeFileUrl &&
+          (loading ? (
+            <div className="mt-6">
+              {skeleton({ widthCls: 'w-40', heightCls: 'h-8' })}
+            </div>
+          ) : (
+            <a
+              href={resumeFileUrl}
+              target="_blank"
+              className="btn btn-outline btn-sm text-xs mt-6 opacity-50"
+              download
+              rel="noreferrer"
+            >
+              Download Resume
+            </a>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default AvatarCard;
+import { FALLBACK_IMAGE } from '../../constants';
+import { Profile } from '../../interfaces/profile';
+import { skeleton } from '../../utils';
+import LazyImage from '../lazy-image';
 
 interface AvatarCardProps {
   profile: Profile | null;
