1f7df0a645ff438bfa05c10b5127606044b928a5
JI Ratul <72037035+ratul0547@users.noreply.github.com>
2026-05-04 12:43:35 -0500

    Implement theme changer component with toggle functionality

--- src/components/theme-changer/index.tsx
+++ src/components/theme-changer/index.tsx
@@ -1,3 +1,93 @@
+import { RiMoonLine, RiSunLine } from 'react-icons/ri';
+import { SanitizedThemeConfig } from '../../interfaces/sanitized-config';
+import { LOCAL_STORAGE_KEY_NAME } from '../../constants';
+import { skeleton } from '../../utils';
+import { MouseEvent } from 'react';
+
+/**
+ * Renders a theme changer component.
+ *
+ * @param {Object} props - The props object.
+ * @param {string} props.theme - The current theme.
+ * @param {function} props.setTheme - A function to set the theme.
+ * @param {boolean} props.loading - Whether the component is in a loading state.
+ * @param {SanitizedThemeConfig} props.themeConfig - The theme configuration object.
+ * @return {JSX.Element} The rendered theme changer component.
+ */
+const ThemeChanger = ({
+  theme,
+  setTheme,
+  loading,
+  themeConfig,
+}: {
+  theme: string;
+  setTheme: (theme: string) => void;
+  loading: boolean;
+  themeConfig: SanitizedThemeConfig;
+}) => {
+  const toggleTheme = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const selectedTheme = theme === 'light' ? 'dark' : 'light';
+
+    document.querySelector('html')?.setAttribute('data-theme', selectedTheme);
+
+    typeof window !== 'undefined' &&
+      localStorage.setItem(LOCAL_STORAGE_KEY_NAME, selectedTheme);
+
+    setTheme(selectedTheme);
+  };
+
+  return (
+    <div className="card overflow-visible shadow-lg card-sm bg-base-100">
+      <div className="flex-row items-center space-x-4 flex pl-6 pr-2 py-4">
+        <div className="flex-1">
+          <h5 className="card-title">
+            {loading ? (
+              skeleton({
+                widthCls: 'w-20',
+                heightCls: 'h-8',
+                className: 'mb-1',
+              })
+            ) : (
+              <span className="text-base-content opacity-70">Theme</span>
+            )}
+          </h5>
+          <span className="text-base-content/50 capitalize text-sm">
+            {loading
+              ? skeleton({ widthCls: 'w-16', heightCls: 'h-5' })
+              : theme === themeConfig.defaultTheme
+                ? 'Default'
+                : theme}
+          </span>
+        </div>
+        <div className="flex-0">
+          {loading ? (
+            skeleton({
+              widthCls: 'w-12',
+              heightCls: 'h-10',
+              className: 'mr-6',
+            })
+          ) : (
+            <button
+              onClick={toggleTheme}
+              title="Toggle Theme"
+              className="btn btn-ghost m-1 normal-case opacity-50 text-base-content flex items-center whitespace-nowrap"
+            >
+              {theme === 'light' ? (
+                <RiMoonLine className="inline-block w-5 h-5 stroke-current" />
+              ) : (
+                <RiSunLine className="inline-block w-5 h-5 stroke-current" />
+              )}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeChanger;
 import { RiDice4Line } from 'react-icons/ri';
 import { SanitizedThemeConfig } from '../../interfaces/sanitized-config';
 import { LOCAL_STORAGE_KEY_NAME } from '../../constants';
